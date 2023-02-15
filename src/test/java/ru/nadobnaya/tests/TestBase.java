d1cd7012b54b5281e92bad0a763fa9d68a80257d
JennyNdbn <NadobnayaEA@yandex.ru>
2023-02-15 23:37:44 +0700

    added listener AllureSelenide

--- src/test/java/ru/nadobnaya/tests/TestBase.java
+++ src/test/java/ru/nadobnaya/tests/TestBase.java
@@ -2,8 +2,11 @@ package ru.nadobnaya.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import ru.nadobnaya.helpers.Attach;
 import ru.nadobnaya.pages.*;
@@ -38,9 +41,16 @@ public class TestBase {
         Configuration.remote = System.getProperty("remote");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("selenoid:options", Map.<String, Object>of("enableVNC", true, "enableVideo", true));
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true));
 
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
